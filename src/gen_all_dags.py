cc1b566c4a3ae507142b096449ec2b696ee67963
Rory Creedon <rory.s.creedon@gmail.com>
2023-08-13 21:08:38 +0100

    Updated dag generation

--- src/gen_all_dags.py
+++ src/gen_all_dags.py
@@ -1,10 +1,7 @@
 from numba import jit
 import numpy as np
-
 from itertools import product
 import time
-
-from tqdm import tqdm
 
 
 @jit(nopython=True)
@@ -28,33 +25,32 @@ def has_cycle_optimized(matrix, node, visited, current_path):
     return False
 
 
-def add_edges(matrix, row, N):
-    results = []
+def add_edges_generator_fixed(matrix, row, N):
+    """A generator version of the add_edges function."""
     if row == N:
-        results.append(matrix.copy())
-        return results
+        yield matrix.copy()
+        return
 
     for values in product([0, 1], repeat=N):
-        matrix[row, :] = values
+        new_matrix = matrix.copy()
+        new_matrix[row, :] = values
         if not any(
             has_cycle_optimized(
-                matrix, node, np.zeros(N, dtype=np.bool_), np.zeros(N, dtype=np.bool_)
+                new_matrix,
+                node,
+                np.zeros(N, dtype=np.bool_),
+                np.zeros(N, dtype=np.bool_),
             )
             for node in range(N)
         ):
-            results.extend(add_edges(matrix, row + 1, N))
-    return results
+            yield from add_edges_generator_fixed(new_matrix, row + 1, N)
 
 
-def generate_dags(N):
-    matrix = np.zeros((N, N), dtype=np.int8)
-    return add_edges(matrix, 0, N)
+def dag_generator(N):
+    return add_edges_generator_fixed(np.zeros((N, N), dtype=np.int8), 0, N)
 
 
 if __name__ == "__main__":
     # Usage
-    start = time.time()
-    dags_3_optimized = generate_dags(15)
-    end = time.time()
-    print(f"Took {end - start} seconds")
-    print(len(dags_3_optimized))
+    all_dags = dag_generator(5)
+    print(len(list(all_dags)))
